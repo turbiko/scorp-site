38c8503b7db24ca0655e1e3d955386f0b912a7da
Andriy Voznyuk <andreyv@ukr.net>
2024-06-10 02:05:05 +0200

    register models in django admin

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from projects.models import Genre, ProjectGenres, Project
+
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(ProjectGenres)
+admin.site.register(Project)
+
