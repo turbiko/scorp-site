ecc88a71a5cc01c57117202d480aa4e70f7515dc
Andriy Voznyuk <andreyv@ukr.net>
2025-02-18 09:35:11 +0100

    add import from env allowed-hosts

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -1,5 +1,5 @@
 from .base import *
-
+from decouple import Csv
 import logging
 
 
@@ -14,18 +14,24 @@ logger.info("Loaded DEVELOPER settings.")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-+$d#&btb)wb$71i1#0eellbko%#m1+bfm^ek+%(h^9hqo8od9d"
 
-ALLOWED_HOSTS = [
-    "selected12309.svitlo.tv",
-    "svitlo.tv",
-    "10.1.100.174",
-    "127.0.0.1",
-    "localhost",
-]
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# if not ALLOWED_HOSTS:
+#     ALLOWED_HOSTS = [
+#         "127.0.0.1",
+#         "localhost",
+#     ]
+
+
 print(f"Allowed hosts dev:{ALLOWED_HOSTS}")
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.svitlo.tv',
-    'http://10.1.100.173',
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://*.domain.tld',
+#     'http://111.111.100.111',
+# ]
+
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
+print(f"CSRF_TRUSTED_ORIGINS dev:{CSRF_TRUSTED_ORIGINS}")
+
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
